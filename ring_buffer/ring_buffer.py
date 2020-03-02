53d97313be7a8ecf491fec84823cd8e1073a204c
samuel <osamaimafidon@gmail.com>
2020-03-02 01:06:18 +0100

    mvp and all stretch

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,31 +2,52 @@ from doubly_linked_list import DoublyLinkedList
 
 
 class RingBuffer:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.current = None
-        self.storage = DoublyLinkedList()
+  def __init__(self, capacity):
+    self.capacity = capacity
+    self.current = None
+    self.storage = DoublyLinkedList()
 
-    def append(self, item):
-        pass
+  def __len__(self):
+    return self.storage.length
 
-    def get(self):
-        # Note:  This is the only [] allowed
-        list_buffer_contents = []
+  def append(self, item):
+    # while there space is available, add to tail, and update current
+    if self.storage.length < self.capacity:
+      self.storage.add_to_tail(item)
+      self.current = self.storage.tail
+    # if theres no more space 
+    if self.storage.length == self.capacity:
+      # update current value
+      self.current.value = item
+      # if we're currently at the tail
+      if self.current is self.storage.tail:
+          # go back to head 
+          self.current = self.storage.head
+      else:
+          # else, go to the next
+          self.current = self.current.next
 
-        # TODO: Your code here
+  def get(self):
+    # Note:  This is the only [] allowed
+    list_buffer_contents = []
 
-        return list_buffer_contents
+    # TODO: Your code here
+    node = self.storage.head
+    while node != None:
+      if node.value is not None:
+        list_buffer_contents.append(node.value)
+      node = node.next
+    return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
-
-
 class ArrayRingBuffer:
-    def __init__(self, capacity):
-        pass
+  def __init__(self, capacity):
+    self.storage = RingBuffer(capacity)
+    for _ in range(0, capacity):
+      self.storage.append(None)
+  
+  def append(self, item):
+    return self.storage.append(item)
 
-    def append(self, item):
-        pass
-
-    def get(self):
-        pass
+  def get(self):
+    return self.storage.get()
